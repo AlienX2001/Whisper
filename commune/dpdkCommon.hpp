67254dc5c33847682397b88796da7f84c21cc1f8
AlienX <64488123+AlienX2001@users.noreply.github.com>
2022-04-09 12:22:01 +0530

    Probably the 1st file which runs
    Probably the 1st file which runs since its initializing the namespace of Whisper

--- commune/dpdkCommon.hpp
+++ commune/dpdkCommon.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+// meaning this file will be included only once during compilation 
 #include <stdlib.h>
 #include <signal.h>
 #include <getopt.h>
@@ -31,16 +31,16 @@
 using namespace std;
 using namespace pcpp;
 
-
+// making the Whisper namespace (probably the first file to be excuted)
 namespace Whisper
 {
 
 
-using nic_queue_id_t = uint16_t;
-using nic_port_id_t = uint16_t;
-using cpu_core_id_t = uint16_t;
-using mem_pool_size_t = uint16_t;
-using parser_queue_assign_t = map<DpdkDevice *, vector<nic_queue_id_t> > ;
+using nic_queue_id_t = uint16_t; //changing datatypes?
+using nic_port_id_t = uint16_t; //changing datatypes?
+using cpu_core_id_t = uint16_t; //changing datatypes?
+using mem_pool_size_t = uint16_t; //changing datatypes?
+using parser_queue_assign_t = map<DpdkDevice *, vector<nic_queue_id_t> > ; //changing datatypes?
 
 
 struct DpdkConfig final {
@@ -49,16 +49,18 @@ struct DpdkConfig final {
 
     parser_queue_assign_t nic_queue_list;
 
-	DpdkConfig() : core_id(MAX_NUM_OF_CORES + 1) {}
-    virtual ~DpdkConfig() {}
-    DpdkConfig & operator=(const DpdkConfig &) = delete;
-    DpdkConfig(const DpdkConfig &) = delete;
+	DpdkConfig() : core_id(MAX_NUM_OF_CORES + 1) {} //constructor for the struct, but setting core_id to max numbers of core + 1?
+    virtual ~DpdkConfig() {} //destructor of the struct
+    DpdkConfig & operator=(const DpdkConfig &) = delete; //??
+    DpdkConfig(const DpdkConfig &) = delete; //??
 
     void add_nic_queue(parser_queue_assign_t::key_type k, parser_queue_assign_t::mapped_type v) {
+	    // function to add stuff in the nic queue list map
         nic_queue_list.emplace(k, v);
     }
 
     void add_nic_queue(parser_queue_assign_t::value_type p) {
+	    // function to add stuff in the nic queue list map
         nic_queue_list.insert(p);
     }
 
@@ -67,19 +69,20 @@ struct DpdkConfig final {
 
 struct PacketMetaData final {
 
-	uint32_t address;
-	uint16_t proto_code;
-	uint16_t pkt_length;
-	double time_stamp;
+	uint32_t address; // IP Adrress 
+	uint16_t proto_code; // ??
+	uint16_t pkt_length; // packet length
+	double time_stamp; // time stamp
 
-	PacketMetaData() {};
+	PacketMetaData() {}; // blank constructor if no params passed
 
 	explicit PacketMetaData(uint32_t a, uint16_t t, uint16_t l, double ts):
 			address(a), proto_code(t), pkt_length(l), time_stamp(ts) {}
+	// explicit consturctor if params passed so that it implicitly doesnt change the data type or size
 	
-	virtual ~PacketMetaData() {};
-    PacketMetaData & operator=(const PacketMetaData &) = default;
-    PacketMetaData(const PacketMetaData &) = default;
+	virtual ~PacketMetaData() {}; // destructor of the struct
+    PacketMetaData & operator=(const PacketMetaData &) = default; //??
+    PacketMetaData(const PacketMetaData &) = default; //??
 
 };
 
